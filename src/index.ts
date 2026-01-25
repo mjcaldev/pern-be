bbf4b33077ab90f56ec66eb3da2b2d7c418d5143
Micheal Callaghan <mehaul96@gmail.com>
2026-01-25 13:11:15 -0500

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
 import express from 'express';
 import subjectsRouter from './routes/subjects';
+import cors from 'cors';
 
 const app = express();
 const PORT = 8000;
+
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+
+}))
 
 app.use(express.json());
 
